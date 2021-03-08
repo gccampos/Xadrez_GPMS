9dd1311f9cd3d4bbfd1cbf6271cc664effadfddb
Guilherme Campos <gcordeiro@id.uff.br>
2021-03-08 16:34:23 -0300

    Implementação do controle da máquina de estados

---
+++ Assets/Scripts/StateMachine/LoadState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadState : State{
+    public override void Enter(){
+        Debug.Log("entrou loadState");
+    }
+
+
+}
