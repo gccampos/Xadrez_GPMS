d9aaef006bb1ef1bd927e78ec233aba188c6c2ba
Gabriel Sena <gabrieloddone@gmail.com>
2021-04-03 22:51:19 -0300

    Animação de Movimentação da Peça

--- Assets/Scripts/StateMachine/States/PieceMovementState.cs
+++ Assets/Scripts/StateMachine/States/PieceMovementState.cs
@@ -7,9 +7,9 @@ public class PieceMovementState : State
 {
     public override async void Enter(){
             Piece piece= Chessboard.instance.selectedPiece;
-            Vector2 pos=Chessboard.instance.selectedHighlight.transform.position;
-            pos.y-=1;
-            piece.transform.position=pos;
+            //Vector2 pos=Chessboard.instance.selectedHighlight.transform.position;
+            //pos.y-=1;
+            //piece.transform.position=pos;
             piece.tile.content=null;
             piece.tile=Chessboard.instance.selectedHighlight.tile;
             if(piece.tile.content!=null){
@@ -18,7 +18,14 @@ public class PieceMovementState : State
                 deadPiece.gameObject.SetActive(false);
             }
             piece.tile.content=piece;
-            await Task.Delay(100);
+
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+            float timing = Vector3.Distance(piece.transform.position, Chessboard.instance.selectedHighlight.transform.position)*0.5f;
+            LeanTween.move(piece.gameObject, Chessboard.instance.selectedHighlight.transform.position, 1.5f).
+                setOnComplete(()=> {
+                    tcs.SetResult(true);
+                });
+            await tcs.Task;
             machine.ChangeTo<TurnEndState>();
     }
 }
