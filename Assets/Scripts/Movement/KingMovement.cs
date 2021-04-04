be119004728ce1cd4a9df3482a1242614815f6ba
Gabriel Sena <gabrieloddone@gmail.com>
2021-04-04 15:41:28 -0300

    Movimentação da Rainha

--- Assets/Scripts/Movement/KingMovement.cs
+++ Assets/Scripts/Movement/KingMovement.cs
@@ -6,17 +6,17 @@ public class KingMovement : Movement
 {
     public override List<Tile> GetValidMoves(){
         List<Tile> moves = new List<Tile>();
-        moves.AddRange(UntilBlockedPath(new Vector2Int(1, 0, true, 1)));
-        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, 0, true, 1)));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(1, 0), true, 1));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, 0), true, 1));
 
-        moves.AddRange(UntilBlockedPath(new Vector2Int(0, 1, true, 1)));
-        moves.AddRange(UntilBlockedPath(new Vector2Int(0, -1, true, 1)));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(0, 1), true, 1));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(0, -1), true, 1));
 
-        moves.AddRange(UntilBlockedPath(new Vector2Int(1, 1, true, 1)));
-        moves.AddRange(UntilBlockedPath(new Vector2Int(1, -1, true, 1)));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(1, 1), true, 1));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(1, -1), true, 1));
 
-        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, -1, true, 1)));
-        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, 1, true, 1)));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, -1), true, 1));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, 1), true, 1));
 
         return moves;
     }
