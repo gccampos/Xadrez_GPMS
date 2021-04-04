796bb09b323757f31a48aae03d6f17794337b47e
Gabriel Sena <gabrieloddone@gmail.com>
2021-04-04 15:59:17 -0300

    Movimentação do Cavalo

---
+++ Assets/Scripts/Movement/KnightMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightMovement : Movement
+{
+    public override List<Tile> GetValidMoves(){
+        List<Tile> moves = new List<Tile>();
+
+        moves.AddRange(GetMovement(new Vector2Int(0, 1)));
+        moves.AddRange(GetMovement(new Vector2Int(0, -1)));
+        moves.AddRange(GetMovement(new Vector2Int(1, 0)));
+        moves.AddRange(GetMovement(new Vector2Int(-1, 0)));
+
+        return moves;
+    }
+
+    List<Tile> GetMovement(Vector2Int direction){
+        List<Tile> moves = new List<Tile>();
+        Tile current = Chessboard.instance.selectedPiece.tile;
+        Tile temp = GetTile(current.pos+direction*2);
+        if(temp!=null){
+            moves.AddRange(GetCurvedPart(temp.pos, new Vector2Int(direction.y, direction.x)));
+        }
+        return moves;
+    }
+
+    List<Tile> GetCurvedPart(Vector2Int pos, Vector2Int direction){
+        List<Tile> tiles = new List<Tile>();
+        Tile tile1 = GetTile(pos+direction);
+        Tile tile2 = GetTile(pos-direction);
+        if(tile1 != null){
+            tiles.Add(tile1);
+        }
+        if(tile2 != null){
+            tiles.Add(tile2);
+        }     
+        return tiles;
+    }
+
+}
--- Assets/Scripts/pieces/knight.cs
+++ Assets/Scripts/pieces/knight.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class Knight : Piece
 {
-
+    void Awake(){
+        movement = new KnightMovement();
+    }
 }
