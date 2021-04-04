86e755b3be8f70f5c173c9942168084cf7dd3be5
Gabriel Sena <gabrieloddone@gmail.com>
2021-04-04 15:45:00 -0300

    Movimentação da Torre

---
+++ Assets/Scripts/Movement/RookMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RookMovement : Movement
+{
+    public override List<Tile> GetValidMoves(){
+        List<Tile> moves = new List<Tile>();
+        moves.AddRange(UntilBlockedPath(new Vector2Int(1, 0), true, 99));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(-1, 0), true, 99));
+
+        moves.AddRange(UntilBlockedPath(new Vector2Int(0, 1), true, 99));
+        moves.AddRange(UntilBlockedPath(new Vector2Int(0, -1), true, 99));
+
+        return moves;
+    }
+}
--- Assets/Scripts/pieces/Rook.cs
+++ Assets/Scripts/pieces/Rook.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class Rook : Piece
 {
-
+    void Awake(){
+        movement = new RookMovement();
+    }
 }
