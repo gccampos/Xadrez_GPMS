5f5e9e654e093269bc6e62f17c4ad6bf6624bc2f
Guilherme Campos <gcordeiro@id.uff.br>
2021-04-16 13:37:48 -0300

    Implementação de castling(rook)

--- Assets/Scripts/StateMachine/States/TurnEndState.cs
+++ Assets/Scripts/StateMachine/States/TurnEndState.cs
@@ -6,13 +6,20 @@ public class TurnEndState : State
 {
    public override async void Enter()
     {
-        //Debug.Log("TurnEndState");
-        bool gameFinished = CheckTeams();
+        Debug.Log("entrou TurnEndState");
+        bool gameFinished = CheckConditions();
         await Task.Delay(100);
         if (gameFinished)
             machine.ChangeTo<GameEndState>();
         else
             machine.ChangeTo<TurnBeginState>();
+    }
+
+    bool CheckConditions(){
+        if(CheckTeams() || CheckKing()){
+            return true;
+        }
+        return false;
     }
 
     bool CheckTeams()
@@ -27,6 +34,20 @@ public class TurnEndState : State
         if (greenPiece == null)
         {
             Debug.Log("Lado dourado ganhou");
+            return true;
+        }
+        return false;
+    }
+    
+    bool CheckKing(){
+        King king = Chessboard.instance.goldHolder.GetComponentInChildren<King>();
+        if(king == null){
+            Debug.Log("Lado Verde Ganhou");
+            return true;
+        }
+        king = Chessboard.instance.greenHolder.GetComponentInChildren<King>();
+        if(king == null){
+            Debug.Log("Lado Dourado Ganhou");
             return true;
         }
         return false;
